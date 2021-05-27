84ec7994e72c4fb379118f7ce26519783a1f8bea
Kaituo Li <kaituo@amazon.com>
2021-05-27 13:00:27 -0700

    Remove error in the state when there is no error anymore.
    Previously, we won't reset error in the state when a detector changes from an error state to a non-error state. This PR fixes that.
    
    Testing done:
    * Tested on a cluster the problem is gone with the fix.

--- src/main/java/org/opensearch/ad/transport/handler/DetectionStateHandler.java
+++ src/main/java/org/opensearch/ad/transport/handler/DetectionStateHandler.java
@@ -167,6 +167,7 @@ public class DetectionStateHandler extends AnomalyIndexHandler<DetectorInternalS
                 if (cause instanceof IndexNotFoundException) {
                     super.index(handler.createNewState(null), detectorId);
                 } else {
+                    // e.g., can happen during node reboot
                     LOG.error("Failed to get detector state " + detectorId, exception);
                 }
             }));
