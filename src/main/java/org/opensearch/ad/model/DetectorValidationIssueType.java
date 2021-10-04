37d9eadd3963c0cda840c7471765e4ec2c6d9850
Amit Galitzky <amgalitz@amazon.com>
2021-10-04 18:20:25 +0000

    changed license comments and added some integ tests

--- src/main/java/org/opensearch/ad/model/DetectorValidationIssueType.java
+++ src/main/java/org/opensearch/ad/model/DetectorValidationIssueType.java
@@ -9,25 +9,7 @@
  * GitHub history for details.
  */
 
-/*
- * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 package org.opensearch.ad.model;
-
-import java.util.Collection;
-import java.util.Set;
 
 import org.opensearch.ad.Name;
 
@@ -76,7 +58,7 @@ public enum DetectorValidationIssueType implements Name {
         }
     }
 
-    public static Set<DetectorValidationIssueType> getNames(Collection<String> names) {
-        return Name.getNameFromCollection(names, DetectorValidationIssueType::getName);
-    }
+    // public static Set<DetectorValidationIssueType> getNames(Collection<String> names) {
+    // return Name.getNameFromCollection(names, DetectorValidationIssueType::getName);
+    // }
 }
