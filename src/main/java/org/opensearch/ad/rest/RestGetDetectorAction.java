856d969aaa00c4cc17dfda9a6617522adecc17ad
Frank Lou <mloufra@amazon.com>
2022-11-15 16:54:17 +0000

    add the get action to the list of Rest Handlers
    Signed-off-by: Frank Lou <mloufra@amazon.com>

--- src/main/java/org/opensearch/ad/rest/RestGetDetectorAction.java
+++ src/main/java/org/opensearch/ad/rest/RestGetDetectorAction.java
@@ -1,21 +1,20 @@
 package org.opensearch.ad.rest;
 
 import static org.opensearch.rest.RestRequest.Method.GET;
-import static org.opensearch.rest.RestStatus.OK;
 import static org.opensearch.rest.RestStatus.NOT_FOUND;
+import static org.opensearch.rest.RestStatus.OK;
 
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.opensearch.rest.RestHandler.Route;
-import org.opensearch.rest.RestRequest.Method;
 import org.opensearch.ad.constant.CommonErrorMessages;
 import org.opensearch.ad.settings.EnabledSetting;
 import org.opensearch.extensions.rest.ExtensionRestRequest;
 import org.opensearch.extensions.rest.ExtensionRestResponse;
+import org.opensearch.rest.RestHandler.Route;
+import org.opensearch.rest.RestRequest.Method;
 import org.opensearch.sdk.ExtensionRestHandler;
-
 
 public class RestGetDetectorAction implements ExtensionRestHandler {
     private final Logger logger = LogManager.getLogger(RestGetDetectorAction.class);
@@ -39,8 +38,8 @@ public class RestGetDetectorAction implements ExtensionRestHandler {
                 "Extension REST action improperly configured to handle " + request.toString()
             );
         }
-    // do things with request
-    return new ExtensionRestResponse(request, OK, "placeholder");
+        // do things with request
+        return new ExtensionRestResponse(request, OK, "placeholder");
     }
 
 }
