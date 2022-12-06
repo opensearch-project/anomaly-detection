fd4b6677536907061519b57ce08727c8d22bd914
Varun Jain <varunudr@amazon.com>
2022-12-05 21:20:35 -0800

    Model Profile Test
    Signed-off-by: Varun Jain <varunudr@amazon.com>

--- src/test/java/org/opensearch/ad/model/ModelProfileTests.java
+++ src/test/java/org/opensearch/ad/model/ModelProfileTests.java
@@ -1,3 +1,14 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
 package org.opensearch.ad.model;
 
 import static org.opensearch.common.xcontent.XContentFactory.jsonBuilder;
@@ -20,24 +31,25 @@ public class ModelProfileTests extends AbstractADTest {
             Entity.createSingleAttributeEntity(randomAlphaOfLength(5), randomAlphaOfLength(5)),
             0
         );
-        XContentBuilder builder = jsonBuilder();
-        builder.startObject();
-        profile1.toXContent(builder, ToXContent.EMPTY_PARAMS);
-        builder.endObject();
+        XContentBuilder builder = getBuilder(profile1);
         String json = Strings.toString(builder);
         assertTrue(JsonDeserializer.hasChildNode(json, CommonName.ENTITY_KEY));
         assertFalse(JsonDeserializer.hasChildNode(json, CommonName.MODEL_SIZE_IN_BYTES));
 
         ModelProfile profile2 = new ModelProfile(randomAlphaOfLength(5), null, 1);
 
-        builder = jsonBuilder();
-        builder.startObject();
-        profile2.toXContent(builder, ToXContent.EMPTY_PARAMS);
-        builder.endObject();
+        builder = getBuilder(profile2);
         json = Strings.toString(builder);
 
         assertFalse(JsonDeserializer.hasChildNode(json, CommonName.ENTITY_KEY));
         assertTrue(JsonDeserializer.hasChildNode(json, CommonName.MODEL_SIZE_IN_BYTES));
+    }
 
+    private XContentBuilder getBuilder(ModelProfile profile) throws IOException {
+        XContentBuilder builder = jsonBuilder();
+        builder.startObject();
+        profile.toXContent(builder, ToXContent.EMPTY_PARAMS);
+        builder.endObject();
+        return builder;
     }
 }
