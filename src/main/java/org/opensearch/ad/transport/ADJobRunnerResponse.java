6ef401995b5dd9d69cd4ab8539afb1c71b48b791
Varun Jain <varunudr@amazon.com>
2023-01-15 02:27:39 -0800

    Transport Api calls for ScheduledJobRunner And ScheduledJobParameter

---
+++ src/main/java/org/opensearch/ad/transport/ADJobRunnerResponse.java
@@ -0,0 +1,17 @@
+package org.opensearch.ad.transport;
+
+import java.io.IOException;
+
+import org.opensearch.action.ActionResponse;
+import org.opensearch.common.io.stream.StreamInput;
+import org.opensearch.common.io.stream.StreamOutput;
+
+public class ADJobRunnerResponse extends ActionResponse {
+
+    public ADJobRunnerResponse(StreamInput streamInput) {}
+
+    @Override
+    public void writeTo(StreamOutput streamOutput) throws IOException {
+
+    }
+}
