a5abce2695c5165d06a90767e1194ec28f689b23
Owais Kazi <owaiskazi19@gmail.com>
2023-04-04 13:40:04 -0700

    Spotless Fix
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- src/main/java/org/opensearch/ad/rest/RestDeleteAnomalyDetectorAction.java
+++ src/main/java/org/opensearch/ad/rest/RestDeleteAnomalyDetectorAction.java
@@ -36,7 +36,6 @@ import org.opensearch.common.settings.Settings;
 import org.opensearch.common.xcontent.json.JsonXContent;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 import org.opensearch.core.xcontent.ToXContent;
-import org.opensearch.extensions.rest.ExtensionRestRequest;
 import org.opensearch.extensions.rest.ExtensionRestResponse;
 import org.opensearch.rest.RestRequest;
 import org.opensearch.rest.RestStatus;
