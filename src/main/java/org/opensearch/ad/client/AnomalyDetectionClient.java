23f9f4c13490ebc8537a5004ff56e8d4f6642fda
Tyler Ohlsen <ohltyler@amazon.com>
2023-12-06 01:07:08 +0000

    Remove dup header
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- src/main/java/org/opensearch/ad/client/AnomalyDetectionClient.java
+++ src/main/java/org/opensearch/ad/client/AnomalyDetectionClient.java
@@ -3,11 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 package org.opensearch.ad.client;
 
 import org.opensearch.action.search.SearchRequest;
