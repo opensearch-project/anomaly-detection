4e6ca487bc47e3b1152fd7ab21218cf9cda4ef1f
Tyler Ohlsen <ohltyler@amazon.com>
2023-12-27 14:46:44 -0800

    [Backport 2.x] Add profile transport action to AD client (#1123)
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- src/main/java/org/opensearch/ad/client/AnomalyDetectionNodeClient.java
+++ src/main/java/org/opensearch/ad/client/AnomalyDetectionNodeClient.java
@@ -5,18 +5,29 @@
 
 package org.opensearch.ad.client;
 
+import java.util.function.Function;
+
 import org.opensearch.action.search.SearchRequest;
 import org.opensearch.action.search.SearchResponse;
+import org.opensearch.ad.transport.ADTaskProfileAction;
+import org.opensearch.ad.transport.ADTaskProfileRequest;
+import org.opensearch.ad.transport.ADTaskProfileResponse;
 import org.opensearch.ad.transport.SearchAnomalyDetectorAction;
 import org.opensearch.ad.transport.SearchAnomalyResultAction;
+import org.opensearch.ad.util.DiscoveryNodeFilterer;
 import org.opensearch.client.Client;
+import org.opensearch.cluster.node.DiscoveryNode;
+import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.action.ActionResponse;
 
 public class AnomalyDetectionNodeClient implements AnomalyDetectionClient {
     private final Client client;
+    private final DiscoveryNodeFilterer nodeFilterer;
 
-    public AnomalyDetectionNodeClient(Client client) {
+    public AnomalyDetectionNodeClient(Client client, ClusterService clusterService) {
         this.client = client;
+        this.nodeFilterer = new DiscoveryNodeFilterer(clusterService);
     }
 
     @Override
@@ -37,5 +48,35 @@ public class AnomalyDetectionNodeClient implements AnomalyDetectionClient {
                 searchRequest,
                 ActionListener.wrap(searchResponse -> { listener.onResponse(searchResponse); }, listener::onFailure)
             );
+    }
+
+    @Override
+    public void getDetectorProfile(String detectorId, ActionListener<ADTaskProfileResponse> listener) {
+        final DiscoveryNode[] eligibleNodes = this.nodeFilterer.getEligibleDataNodes();
+        ADTaskProfileRequest profileRequest = new ADTaskProfileRequest(detectorId, eligibleNodes);
+        this.client.execute(ADTaskProfileAction.INSTANCE, profileRequest, getADTaskProfileResponseActionListener(listener));
+    }
+
+    // We need to wrap AD-specific response type listeners around an internal listener, and re-generate the response from a generic
+    // ActionResponse. This is needed to prevent classloader issues and ClassCastExceptions when executed by other plugins.
+    private ActionListener<ADTaskProfileResponse> getADTaskProfileResponseActionListener(ActionListener<ADTaskProfileResponse> listener) {
+        ActionListener<ADTaskProfileResponse> internalListener = ActionListener
+            .wrap(profileResponse -> { listener.onResponse(profileResponse); }, listener::onFailure);
+        ActionListener<ADTaskProfileResponse> actionListener = wrapActionListener(internalListener, actionResponse -> {
+            ADTaskProfileResponse response = ADTaskProfileResponse.fromActionResponse(actionResponse);
+            return response;
+        });
+        return actionListener;
+    }
+
+    private <T extends ActionResponse> ActionListener<T> wrapActionListener(
+        final ActionListener<T> listener,
+        final Function<ActionResponse, T> recreate
+    ) {
+        ActionListener<T> actionListener = ActionListener.wrap(r -> {
+            listener.onResponse(recreate.apply(r));
+            ;
+        }, e -> { listener.onFailure(e); });
+        return actionListener;
     }
 }
--- src/test/java/org/opensearch/ad/client/AnomalyDetectionNodeClientTests.java
+++ src/test/java/org/opensearch/ad/client/AnomalyDetectionNodeClientTests.java
@@ -5,18 +5,25 @@
 
 package org.opensearch.ad.client;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.opensearch.ad.TestHelpers.matchAllRequest;
 import static org.opensearch.ad.indices.AnomalyDetectionIndices.ALL_AD_RESULTS_INDEX_PATTERN;
 import static org.opensearch.ad.model.AnomalyDetector.DETECTOR_TYPE_FIELD;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.opensearch.action.search.SearchRequest;
@@ -24,8 +31,17 @@ import org.opensearch.action.search.SearchResponse;
 import org.opensearch.action.support.PlainActionFuture;
 import org.opensearch.ad.HistoricalAnalysisIntegTestCase;
 import org.opensearch.ad.TestHelpers;
+import org.opensearch.ad.constant.CommonName;
+import org.opensearch.ad.model.ADTaskProfile;
 import org.opensearch.ad.model.AnomalyDetector;
 import org.opensearch.ad.model.AnomalyDetectorType;
+import org.opensearch.ad.transport.ADTaskProfileAction;
+import org.opensearch.ad.transport.ADTaskProfileNodeResponse;
+import org.opensearch.ad.transport.ADTaskProfileResponse;
+import org.opensearch.client.Client;
+import org.opensearch.cluster.ClusterName;
+import org.opensearch.cluster.node.DiscoveryNode;
+import org.opensearch.core.action.ActionListener;
 import org.opensearch.index.query.BoolQueryBuilder;
 import org.opensearch.index.query.TermQueryBuilder;
 import org.opensearch.search.builder.SearchSourceBuilder;
@@ -37,22 +53,26 @@ import com.google.common.collect.ImmutableList;
 // The exhaustive set of transport action scenarios are within the respective transport action
 // test suites themselves. We do not want to unnecessarily duplicate all of those tests here.
 public class AnomalyDetectionNodeClientTests extends HistoricalAnalysisIntegTestCase {
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     private String indexName = "test-data";
     private Instant startTime = Instant.now().minus(2, ChronoUnit.DAYS);
+    private Client clientSpy;
     private AnomalyDetectionNodeClient adClient;
-    private PlainActionFuture<SearchResponse> future;
+    private PlainActionFuture<SearchResponse> searchResponseFuture;
+    private PlainActionFuture<ADTaskProfileResponse> profileFuture;
 
     @Before
     public void setup() {
-        adClient = new AnomalyDetectionNodeClient(client());
+        clientSpy = spy(client());
+        adClient = new AnomalyDetectionNodeClient(clientSpy, clusterService());
     }
 
     @Test
     public void testSearchAnomalyDetectors_NoIndices() {
         deleteIndexIfExists(AnomalyDetector.ANOMALY_DETECTORS_INDEX);
 
-        SearchResponse searchResponse = adClient.searchAnomalyDetectors(matchAllRequest()).actionGet(10000);
+        SearchResponse searchResponse = adClient.searchAnomalyDetectors(TestHelpers.matchAllRequest()).actionGet(10000);
         assertEquals(0, searchResponse.getInternalResponse().hits().getTotalHits().value);
     }
 
@@ -61,13 +81,13 @@ public class AnomalyDetectionNodeClientTests extends HistoricalAnalysisIntegTest
         deleteIndexIfExists(AnomalyDetector.ANOMALY_DETECTORS_INDEX);
         createDetectorIndex();
 
-        SearchResponse searchResponse = adClient.searchAnomalyDetectors(matchAllRequest()).actionGet(10000);
+        SearchResponse searchResponse = adClient.searchAnomalyDetectors(TestHelpers.matchAllRequest()).actionGet(10000);
         assertEquals(0, searchResponse.getInternalResponse().hits().getTotalHits().value);
     }
 
     @Test
     public void searchAnomalyDetectors_Populated() throws IOException {
-        ingestTestData(indexName, startTime, 1, "test", 3000);
+        ingestTestData(indexName, startTime, 1, "test", 10);
         String detectorType = AnomalyDetectorType.SINGLE_ENTITY.name();
         AnomalyDetector detector = TestHelpers
             .randomAnomalyDetector(
@@ -93,18 +113,18 @@ public class AnomalyDetectionNodeClientTests extends HistoricalAnalysisIntegTest
 
     @Test
     public void testSearchAnomalyResults_NoIndices() {
-        future = mock(PlainActionFuture.class);
+        searchResponseFuture = mock(PlainActionFuture.class);
         SearchRequest request = new SearchRequest().indices(new String[] {});
 
-        adClient.searchAnomalyResults(request, future);
-        verify(future).onFailure(any(IllegalArgumentException.class));
+        adClient.searchAnomalyResults(request, searchResponseFuture);
+        verify(searchResponseFuture).onFailure(any(IllegalArgumentException.class));
     }
 
     @Test
     public void testSearchAnomalyResults_Empty() throws IOException {
         createADResultIndex();
         SearchResponse searchResponse = adClient
-            .searchAnomalyResults(matchAllRequest().indices(ALL_AD_RESULTS_INDEX_PATTERN))
+            .searchAnomalyResults(TestHelpers.matchAllRequest().indices(ALL_AD_RESULTS_INDEX_PATTERN))
             .actionGet(10000);
         assertEquals(0, searchResponse.getInternalResponse().hits().getTotalHits().value);
     }
@@ -116,11 +136,52 @@ public class AnomalyDetectionNodeClientTests extends HistoricalAnalysisIntegTest
         String adResultId = createADResult(TestHelpers.randomAnomalyDetectResult());
 
         SearchResponse searchResponse = adClient
-            .searchAnomalyResults(matchAllRequest().indices(ALL_AD_RESULTS_INDEX_PATTERN))
+            .searchAnomalyResults(TestHelpers.matchAllRequest().indices(ALL_AD_RESULTS_INDEX_PATTERN))
             .actionGet(10000);
-        assertEquals(1, searchResponse.getInternalResponse().hits().getTotalHits().value);
 
+        assertEquals(1, searchResponse.getInternalResponse().hits().getTotalHits().value);
         assertEquals(adResultId, searchResponse.getInternalResponse().hits().getAt(0).getId());
+    }
+
+    @Test
+    public void testGetDetectorProfile_NoIndices() throws ExecutionException, InterruptedException {
+        deleteIndexIfExists(AnomalyDetector.ANOMALY_DETECTORS_INDEX);
+        deleteIndexIfExists(ALL_AD_RESULTS_INDEX_PATTERN);
+        deleteIndexIfExists(CommonName.DETECTION_STATE_INDEX);
+
+        profileFuture = mock(PlainActionFuture.class);
+        ADTaskProfileResponse response = adClient.getDetectorProfile("foo").actionGet(10000);
+        List<ADTaskProfileNodeResponse> responses = response.getNodes();
+
+        assertNotEquals(0, responses.size());
+        assertEquals(null, responses.get(0).getAdTaskProfile());
+        verify(clientSpy, times(1)).execute(any(ADTaskProfileAction.class), any(), any());
+
+    }
+
+    @Test
+    public void testGetDetectorProfile_Populated() {
+        DiscoveryNode localNode = clusterService().localNode();
+        ADTaskProfile adTaskProfile = new ADTaskProfile("foo-task-id", 0, 0L, false, 0, 0L, localNode.getId());
+
+        doAnswer(invocation -> {
+            Object[] args = invocation.getArguments();
+
+            ActionListener<ADTaskProfileResponse> listener = (ActionListener<ADTaskProfileResponse>) args[2];
+            ADTaskProfileNodeResponse nodeResponse = new ADTaskProfileNodeResponse(localNode, adTaskProfile, null);
+
+            List<ADTaskProfileNodeResponse> nodeResponses = Arrays.asList(nodeResponse);
+            listener.onResponse(new ADTaskProfileResponse(new ClusterName("test-cluster"), nodeResponses, Collections.emptyList()));
+
+            return null;
+        }).when(clientSpy).execute(any(ADTaskProfileAction.class), any(), any());
+
+        ADTaskProfileResponse response = adClient.getDetectorProfile("foo").actionGet(10000);
+        String responseTaskId = response.getNodes().get(0).getAdTaskProfile().getTaskId();
+
+        assertNotEquals(0, response.getNodes().size());
+        assertEquals(responseTaskId, adTaskProfile.getTaskId());
+        verify(clientSpy, times(1)).execute(any(ADTaskProfileAction.class), any(), any());
     }
 
 }
